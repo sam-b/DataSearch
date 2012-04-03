2408a0309afb45ffadffb32d7f7e7e1974f9a357
Sam Brown <samdanielbrown@gmail.com>
2012-04-03 16:07:46 +0100

    indexing improved + searching and some kind of ranking

--- Web_Crawler.py
+++ Web_Crawler.py
@@ -35,14 +35,83 @@ def get_all_links(page):
 def crawl_web(seed,max_depth,max_pages):
     tocrawl = [seed]
     crawled = []
+	index = {}
+	graph = {}
     depth = 0
 	pages = 0
     while tocrawl and pages < max_pages:
         page = tocrawl.pop()
         if page not in crawled:
             if depth <max_depth:
-                union(tocrawl, get_all_links(get_page(page)))
+				content = get_page(page)
+				add_page_to_index(index, page, content)
+				links = get_all_links(content)
+				graph[page]=links
+                union(tocrawl, links)
                 depth = depth + 1
             crawled.append(page)
 			pages = pages + 1
-    return crawled 
+    return index, graph 
+	
+def add_page_to_index(index, url, content):
+	split_list = " ,!-"
+    words = split_string(content,split_list);
+    for word in words:
+        add_to_index(index, word, url)
+
+def add_to_index(index, keyword, url):
+        if keyword in index:
+            index[keyword].append(url)
+        else:
+			index[keyword] = [url]
+
+def lookup(index, keyword):
+    if keyword in index:
+		return index[keyword]
+    return None
+	
+def split_string(source,splitlist):
+    string = "";
+    out = [];
+    while True:
+        if source=="":
+            return out;
+        if source[0] not in splitlist:
+            string = string + source[0];
+        if source[0] in splitlist:
+            out.append(string);
+            string = "";
+        source = source[1:];
+		
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+                for node in graph:
+                    if page in graph[node]:
+                        newrand = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+	
+def lucky_search(index, ranks, keyword):
+    best = 0
+    out = ""
+    if keyword not in index:
+        return None
+    for i in index[keyword]:
+        if ranks[i] > best:
+            best = ranks[i]
+            out = i
+    return out
+	
+index, graph = crawl_web("http://www.engimagroup.org",10,100)
+ranks = compute_ranks(graph)
+print lucky_search(index,ranks,"hello")
